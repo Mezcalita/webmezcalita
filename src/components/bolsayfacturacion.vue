0138bf119969098613ba60f9675dc4449d91d2e3
rodrigo mora <rodrigo@lamezcalita.mx>
2020-05-01 12:31:12 -0500

    bolsa y facturacion version movil

--- src/components/bolsayfacturacion.vue
+++ src/components/bolsayfacturacion.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="container-fluid  d-flex    "   >
     <div class="container text-center nomovil " style="max-width:1200px;"> 
         <b-row cols="1" cols-md="2"  no-gutters>
          <b-col>
@@ -48,6 +49,61 @@ Asegúrate de tener el ticket a la mano. </p>
         
         
     </div>
+
+
+<div class="container text-center simovil " style="max-width:600px;"> 
+        <b-row cols="1" cols-md="2"  no-gutters>
+         <b-col>
+
+        <b-card
+       img-src="iconos/bolsayfacturacion/facturacion.svg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    img-width="42"
+    img-height="53"
+   bg-variant="transparent"
+
+  >
+    <b-card-text>
+      <h5 id="titulod">facturación</h5>
+      <p id="parrafoa">¿Quieres facturar tu consumo en La Mezcalita?
+Asegúrate de tener el ticket a la mano. </p>
+    </b-card-text>
+    
+      <div class="mcwidget-embed" data-widget-id="10511068"></div>
+  </b-card>
+
+         </b-col>
+         <b-col>
+
+        <b-card
+       img-src="iconos/bolsayfacturacion/bolsa.svg"
+    img-alt="Image"
+    img-top
+    tag="article"
+        img-width="42"
+    img-height="53"
+ bg-variant="transparent"
+  >
+    <b-card-text>
+            <h5 id="titulod">bolsa de trabajo</h5>
+ <p id="parrafoa">En la Mezcalita seguimos creciendo y buscamos gente como tú para unirse a nuestro equipo de trabajo, deja aquí tu solicitud!</p>
+    </b-card-text>
+    
+        <button class="buttonbordeblanco">VACANTES</button>
+  </b-card>
+
+         </b-col>
+
+        </b-row>
+        
+        
+    </div>
+</div>
+
+
+    
 </template>
 <script>
 export default {
